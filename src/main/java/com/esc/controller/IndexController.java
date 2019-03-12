30e035ae3c5e970b54425a7c51b7b865704edb54
soon <>
2019-03-12 09:36:53 +0900

    lombok 사용할 수 있도록 build.gradle 설정 변경
    as-is : compileOnly
    to-be : compile

--- src/main/java/com/esc/controller/IndexController.java
+++ src/main/java/com/esc/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.esc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class IndexController {
 
---
+++ src/main/java/com/esc/domain/Test.java
@@ -0,0 +1,8 @@
+package com.esc.domain;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test {
+
+}
